f17d19f93e12852e832ae93df0f763c25ca422a6
Roman <letvinoff@gmail.com>
2025-12-09 18:51:21 +0400

    update application according new nrf_edgeai library

---
+++ src/app_version.h
@@ -0,0 +1,14 @@
+/*
+* Copyright (c) 2025 Nordic Semiconductor ASA
+* SPDX-License-Identifier: Apache-2.0
+*/
+#ifndef __APPLICATION_VERSION_H__
+#define __APPLICATION_VERSION_H__
+
+
+#define APP_VERSION_MAJOR    30
+#define APP_VERSION_MINOR    0
+#define APP_VERSION_PATCH    0
+
+
+#endif /* __APPLICATION_VERSION_H__ */
